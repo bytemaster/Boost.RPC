243399c6a4e539dbcccd96431aa0e2db8236ab89
Daniel Larimer <dlarimer@gmail.com>
2012-05-11 18:25:10 -0400

    removing old rpc code

--- include/boost/rpc/json/tcp/server.hpp
+++
@@ -1,9 +0,0 @@
-#include <boost/rpc/json/tcp/connection.hpp>
-
-namespace boost { namespace rpc { namespace json { namespace tcp {
-
-    typedef boost::function<void(const boost::rpc::json::tcp::connection::ptr&)> handler;
-
-    void listen( uint16_t port, const handler& handle );
-
-} } } } // boost::rpc::json::tcp
